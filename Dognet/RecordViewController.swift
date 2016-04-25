73d8732d7099526de9949169fdfe3bc429851455
Jeff Ishaq <jishaq@gmail.com>
2016-04-25 12:38:38 -0700

    Initial Commit

---
+++ Dognet/RecordViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  Dognet
+//
+//  Created by Jeff Ishaq on 4/25/16.
+//  Copyright © 2016 Snowdog Software. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class RecordViewController: UIViewController {
+
+    @IBOutlet weak var map: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
