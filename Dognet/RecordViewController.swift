1d7feb7a50f46502f48e8d480ae74068f69c0405
Jeff Ishaq <jishaq@gmail.com>
2016-04-27 13:42:08 -0700

    Implement CustomStringConvertible protocol upon CLAuthorizationStatus for logging

--- Dognet/RecordViewController.swift
+++ Dognet/RecordViewController.swift
@@ -8,8 +8,35 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class RecordViewController: UIViewController {
+/*
+ * CLAuthorizationStatus Protocol Extension
+ *
+ * Implements CustomStringConverible protocol upon the CLAuthorizationStatus enum, for easy logging
+ * of its enum value.  If this enum were declared in swift, this would happen automatically but
+ * at present, CLLocationManager is Objective C.  For example, in your locationManager(manager,
+ * didChangeAuthorizationStatus) delegate, you can do:
+ *
+ *   print("Authorization status changed to \(status)")
+ *
+ * See Also: https://appventure.me/2015/10/17/advanced-practical-enum-examples/#sec-2-2
+ * See Also: 
+ * JeffI 2016-04-27 Initial implementation
+ */
+extension CLAuthorizationStatus: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .AuthorizedAlways: return "AuthorizedAlways"
+        case .AuthorizedWhenInUse: return "AuthorizedWhenInUse"
+        case .Denied: return "Denied"
+        case .NotDetermined: return "NotDetermined"
+        case .Restricted: return "Restricted"
+        }
+    }
+}
+
+class RecordViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
